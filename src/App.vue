8e10a8df570c64d48170a2c23e0f24eb77980e36
olaffvs <byolaf@live.nl>
2018-01-11 11:23:08 +0100

    added main page

--- src/App.vue
+++ src/App.vue
@@ -14,14 +14,9 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  font-size: 40px;
-}
+<style lang="scss">
+  * {
+    margin: 0;
+  }
 </style>
+
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,53 @@
 <template>
     <body>
-        TEXT HERE
+        <div class="main__container">
+            <div class="main__container-left">
+                test
+            </div>
+            <div class="main__container-right">
+                test
+            </div>
+        </div>
+        <div class="main-title">
+            Welcome.
+        </div>
     </body>
 </template>
-<style lang="scss" scoped>
+<style lang="scss">
+
+    @import url('https://fonts.googleapis.com/css?family=Encode+Sans+Semi+Expanded:500');
+
     body {
-        background-color: hotpink;
-}
+        background: #fff;
+        background-image:
+            linear-gradient(
+            to right,
+                #fff 50%,
+                #ccc 50%
+            );
+    }
+
+    .main__container {
+        display: flex;
+
+        &-left {
+            width: 50%;
+        }
+
+        &-right {
+            width: 50%;
+        }
+    }
+
+    .main-title {
+        left: 0;
+        line-height: 200px;
+        margin-top: -100px;
+        position: absolute;
+        text-align: center;
+        top: 50%;
+        width: 100%;
+        font-size: 5rem;
+        font-family: 'Encode Sans Semi Expanded', sans-serif;
+    }
 </style>
