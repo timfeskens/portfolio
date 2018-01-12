b20588428ccb9512c9638502b45afe0026812573
olaffvs <byolaf@live.nl>
2018-01-12 13:43:10 +0100

    Made homepage

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,13 @@
 <template>
     <body>
-        <div class="main__container">
-            <div class="main__container-left">
-                test
-            </div>
-            <div class="main__container-right">
-                test
-            </div>
-        </div>
-        <div class="main-title">
+        <div class="main__title">
             Welcome.
+        </div>
+        <div class="main__container-image">
+        </div>
+        <div class="main__container-hover">
+        </div>
+        <div class="main__container-hover--2">
         </div>
     </body>
 </template>
@@ -17,37 +15,67 @@
 
     @import url('https://fonts.googleapis.com/css?family=Encode+Sans+Semi+Expanded:500');
 
-    body {
-        background: #fff;
-        background-image:
-            linear-gradient(
-            to right,
-                #fff 50%,
-                #ccc 50%
-            );
-    }
-
-    .main__container {
-        display: flex;
-
-        &-left {
-            width: 50%;
+    .main {
+        &__title {
+            left: 0;
+            line-height: 200px;
+            margin-top: -100px;
+            position: absolute;
+            text-align: center;
+            top: 50%;
+            width: 100%;
+            font-size: 5rem;
+            font-family: 'Encode Sans Semi Expanded', sans-serif;
+            z-index: 20;
+            pointer-events: none;
+            color: #fff;
         }
 
-        &-right {
-            width: 50%;
+        &__container-image {
+            background-image: url("../../bg.jpg");
+            height: 100%;
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 100%;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            z-index: 2;
         }
-    }
 
-    .main-title {
-        left: 0;
-        line-height: 200px;
-        margin-top: -100px;
-        position: absolute;
-        text-align: center;
-        top: 50%;
-        width: 100%;
-        font-size: 5rem;
-        font-family: 'Encode Sans Semi Expanded', sans-serif;
+        &__container-hover {
+            position: absolute;
+            width: 50%;
+            height: 100%;
+            // background-color: hotpink;
+            opacity: .5;
+            z-index: 12;
+            cursor: pointer;
+            background-color: black;
+            transition-duration: 500ms;
+
+            &:hover {
+                opacity: 0;
+                transition-duration: 500ms;
+            }
+        }
+
+        &__container-hover--2 {
+            position: absolute;
+            width: 50%;
+            height: 100%;
+            background-color: black;
+            opacity: .5;
+            z-index: 12;
+            right: 0;
+            cursor: pointer;
+            transition-duration: 500ms;
+
+            &:hover {
+                opacity: 0;
+                transition-duration: 500ms;
+            }
+        }
     }
 </style>
